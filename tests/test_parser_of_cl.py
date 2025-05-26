3ae6934052c6beb0c567fa47a36300910a1bb846
Ek <shigalovaekaterina@gmail.com>
2025-05-26 22:19:14 +0700

    fix lint conditions

--- tests/test_parser_of_cl.py
+++ tests/test_parser_of_cl.py
@@ -1,6 +1,7 @@
 import pytest
 from gendiff.scripts.parser_of_cl import parse_cl
 
+
 def test_parse_cl_default_format(monkeypatch):
     monkeypatch.setattr("sys.argv", ["gendiff", "file1.json", "file2.json"])
     args = parse_cl()
@@ -8,12 +9,15 @@ def test_parse_cl_default_format(monkeypatch):
     assert args.second_file == "file2.json"
     assert args.format == "stylish"
 
+
 def test_parse_cl_custom_format(monkeypatch):
-    monkeypatch.setattr("sys.argv", ["prog", "file1.json", "file2.json", "--format", "plain"])
+    monkeypatch.setattr("sys.argv", ["gendiff", "file1.json", "file2.json", 
+                                    "--format", "plain"])
     args = parse_cl()
     assert args.format == "plain"
 
+
 def test_parse_cl_missing_args(monkeypatch):
-    monkeypatch.setattr("sys.argv", ["prog"])
+    monkeypatch.setattr("sys.argv", ["gendiff"])
     with pytest.raises(SystemExit):
         parse_cl()
